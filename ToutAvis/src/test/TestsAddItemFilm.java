7bbb43850b78bf9a15270b8b8fb4c681284cda2f
Jonathan <jonathanquigley@orange.fr>
2015-04-27 00:29:54 +0200

    TestAddItemFilm : version v1

--- ToutAvis/src/test/TestsAddItemFilm.java
+++ ToutAvis/src/test/TestsAddItemFilm.java
@@ -21,7 +21,7 @@ public class TestsAddItemFilm {
 
 
 
-	public static int addItemFilmBadEntryTest (SocialNetwork sn, String pseudo, String password, String titre, String genre, String realisateur, String scenariste){
+	public static int addItemFilmBadEntryTest (SocialNetwork sn, String pseudo, String pwd, String titre, String genre, String realisateur, String scenariste, String idTest, String messErreur){
 		// vérifie que l'ajout d'un film est refusée (levée de l'exception BadEntry et pas de modification de sn)
 		// ne fait rien si c'est le cas
 		// sinon, affiche le message d'erreur passé en paramètre
@@ -29,66 +29,86 @@ public class TestsAddItemFilm {
 		
 		int nbFilms = sn.nbFilms();
 		try {
-			sn.addMember (pseudo, pwd, profil);
+			sn.addItemFilm (pseudo, pwd, titre, genre, realisateur, scenariste, nbFilms);
 			System.out.println ("Test " + idTest + " : " + messErreur);
 			return 1;
 		}
 		catch (BadEntry e) {
-			if (sn.nbMembers() != nbMembres) {
-				System.out.println("Test " + idTest + " : l'exception BadEntry a bien √©t√© lev√©e mais le nombre de membres a √©t√© modifi√©");
+			if (sn.nbFilms() != nbFilms) {
+				System.out.println("Test " + idTest + " : l'exception BadEntry a bien été levée mais le nombre de films a été modifié");
 				return 1;
 			}
 			else 
 				return 0;
 		}
 		catch (Exception e) {
-			System.out.println ("Test " + idTest + " : exception non pr√©vue. " + e); 
+			System.out.println ("Test " + idTest + " : exception non prévue. " + e); 
 			e.printStackTrace();
 			return 1;
 		}
 	}
 
-	public static int addMemberOKTest (SocialNetwork sn, String pseudo, String pwd, String profil, String idTest){
-		int nbMembres = sn.nbMembers();
+	public static int addItemFilmOKTest (SocialNetwork sn, String pseudo, String pwd, String titre, String genre, String realisateur,String scenariste, String idTest){
+		int nbFilms = sn.nbFilms();
 		try{
-			sn.addMember (pseudo, pwd, profil);
-			if (sn.nbMembers() != nbMembres+1) {
-				System.out.println("Test " + idTest + " :  le nombre de membres n'a pas √©t√© correctement incr√©ment√©");
+			sn.addItemFilm (pseudo, pwd, titre, genre, realisateur, scenariste, nbFilms);
+			if (sn.nbFilms() != nbFilms+1) {
+				System.out.println("Test " + idTest + " :  le nombre de films n'a pas été correctement incrémenté");
 				return 1;
 			}
 			else 
 				return 0;
 		}
 		catch (Exception e) {
-			System.out.println ("Test " + idTest + " : exception non pr√©vue. " + e); 
+			System.out.println ("Test " + idTest + " : exception non prévue. " + e); 
 			e.printStackTrace();
 			return 1;
 		}
 	}
 
-	public static int addMemberAlreadyExistsTest (SocialNetwork sn, String pseudo, String pwd, String profil, String idTest, String messErreur){
-		int nbMembres = sn.nbMembers();
+	public static int addItemFilmAlreadyExistsTest (SocialNetwork sn, String pseudo, String pwd, String titre, String genre, String realisateur,String scenariste, String idTest, String messErreur){
+		int nbFilms = sn.nbFilms();
 		try {
-			sn.addMember (pseudo, pwd, profil);
+			sn.addItemFilm (pseudo, pwd, titre, genre, realisateur, scenariste, nbFilms);
 			System.out.println ("Test " + idTest + " : " + messErreur);
 			return 1;
 		}
-		catch (MemberAlreadyExists e) {
-			if (sn.nbMembers() != nbMembres) {
-				System.out.println("Test " + idTest + " : l'exception MemberAlreadyExists a bien √©t√© lev√©e mais le nombre de membres a √©t√© modifi√©");
+		catch (ItemFilmAlreadyExists e) {
+			if (sn.nbFilms() != nbFilms) {
+				System.out.println("Test " + idTest + " : l'exception ItemFilmAlreadyExists a bien été levée mais le nombre de films a été modifié");
 				return 1;
 			}
 			else
 				return 0;
 		}
 		catch (Exception e) {
-			System.out.println ("Test " + idTest + " : exception non pr√©vue. " + e); 
+			System.out.println ("Test " + idTest + " : exception non prévue. " + e); 
 			e.printStackTrace();
 			return 1;
 		}
 	}
 
-
+	public static int addItemFilmNotMemberTest (SocialNetwork sn, String pseudo, String pwd, String titre, String genre, String realisateur,String scenariste, String idTest, String messErreur){
+		int nbFilms = sn.nbFilms();
+		try {
+			sn.addItemFilm (pseudo, pwd, titre, genre, realisateur, scenariste, nbFilms);
+			System.out.println ("Test " + idTest + " : " + messErreur);
+			return 1;
+		}
+		catch (NotMember e) {
+			if (sn.nbFilms() != nbFilms) {
+				System.out.println("Test " + idTest + " : l'exception NotMember a bien été levée mais le nombre de films a été modifié");
+				return 1;
+			}
+			else
+				return 0;
+		}
+		catch (Exception e) {
+			System.out.println ("Test " + idTest + " : exception non prévue. " + e); 
+			e.printStackTrace();
+			return 1;
+		}
+	}
 
 
 	public static void main(String[] args) {
@@ -99,63 +119,82 @@ public class TestsAddItemFilm {
 		int nbTests = 0;
 		int nbErreurs = 0;
 		
-		System.out.println("Tests  ajouter des membres au r√©seau social  ");
+		System.out.println("Tests  ajouter des films au réseau social  ");
 
 
 		SocialNetwork sn = new SocialNetwork();
 
-		// tests de addMember
+		//instanciation de quatre membres
+		
+		try {
+			sn.addMember("jacques", "qsdfgh", "Amateur");
+			sn.addMember("Jean-Mi", "jeanjean", "Paysan");
+			sn.addMember("Gaetan", "azerty", "Ivrogne");
+			sn.addMember("Mick", "12345", "Prof à télécom bretagne");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Exception inattendue levée lors de l'utilisation de addMember");
+		}
+		
+		// tests de addItemFilm
 		nbFilms = sn.nbFilms();
 		nbLivres = sn.nbBooks();
 
-		// <=> fiche num√©ro 1
+		// <=> fiche numéro 3
 
-		// tentative d'ajout de membres avec entr√©es "incorrectes"
+		// tentative d'ajout de films avec entrées "incorrectes"
 
 		nbTests++;
-		nbErreurs += addMemberBadEntryTest ( sn, null, "qsdfgh", "", "1.1", "L'ajout d'un membre dont le pseudo n'est pas instanci√© est accept√©");
+		nbErreurs += addItemFilmBadEntryTest ( sn, null, "qsdfgh", "titre", "genre", "realisateur", "scenariste", "3.1", "L'ajout d'un film avec un pseudo n'est pas instancié est accepté");
 		nbTests++;
-		nbErreurs += addMemberBadEntryTest ( sn, " ", "qsdfgh", "", "1.2", "L'ajout d'un membre dont le pseudo ne contient pas un caracteres, autre que des espaces, est accept√©");
+		nbErreurs += addItemFilmBadEntryTest ( sn, "   ", "qsdfgh", "titre", "genre", "realisateur", "scenariste", "3.2", "L'ajout d'un film avec un pseudo qui ne contient que des espaces est accepté");
 		nbTests++;
-		nbErreurs += addMemberBadEntryTest ( sn, "B", null, "", "1.3", "L'ajout d'un membre dont le password n'est pas instanci√© est accept√©");
+		nbErreurs += addItemFilmBadEntryTest ( sn, "jacques", "qsdfgh", null, "genre", "realisateur", "scenariste", "3.3", "L'ajout d'un film dont le titre n'est pas instancié est accepté");
 		nbTests++;
-		nbErreurs += addMemberBadEntryTest ( sn, "B", "   qwd ", "", "1.4", "L'ajout d'un membre dont le password ne contient pas au moins 4 caracteres, autre que des espaces de d√©but ou de fin, est accept√©");
+		nbErreurs += addItemFilmBadEntryTest ( sn, "jacques", "qsdfgh", "  ", "genre", "realisateur", "scenariste", "3.4", "L'ajout d'un film dont le titre ne contient que des espaces est accepté");
 		nbTests++;
-		nbErreurs += addMemberBadEntryTest ( sn, "BBBB", "bbbb", null, "1.5", "L'ajout d'un membre dont le profil n'est pas instanci√© est accept√©");
+		nbErreurs += addItemFilmBadEntryTest ( sn, "jacques", "qsdfgh", "titre", null, "realisateur", "scenariste", "3.5", "L'ajout d'un film dont le genre n'est pas instancié est accepté");
+		nbTests++;
+		nbErreurs += addItemFilmBadEntryTest ( sn, "jacques", "qsdfgh", "titre", "  ", "realisateur", "scenariste", "3.6", "L'ajout d'un film dont le genre ne contient que des espaces est accepté");
+		nbTests++;
+		nbErreurs += addItemFilmBadEntryTest ( sn, "jacques", "qsdfgh", "titre", "genre", null, "scenariste", "3.7", "L'ajout d'un film dont le realisateur n'est pas instancié est accepté");
+		nbTests++;
+		nbErreurs += addItemFilmBadEntryTest ( sn, "jacques", "qsdfgh", "titre", "genre", "  ", "scenariste", "3.8", "L'ajout d'un film dont le realisateur ne contient que des espaces est accepté");
+		nbTests++;
+		nbErreurs += addItemFilmBadEntryTest ( sn, "jacques", "qsdfgh", "titre", "genre", "realisateur", null, "3.9", "L'ajout d'un film dont le scenariste n'est pas instancié est accepté");
+		nbTests++;
+		nbErreurs += addItemFilmBadEntryTest ( sn, "jacques", "qsdfgh", "titre", "genre", "realisateur", "  ", "3.10", "L'ajout d'un film dont le scenariste ne contient que des espaces est accepté");
 
-
-
-		// <=> fiche num√©ro 2
+		
+		// <=> fiche numéro 4
 
 		// ajout de 3 membres avec entr√©es "correctes"
 
 		nbTests++;
-		nbErreurs += addMemberOKTest (sn, "Paul", "paul", "lecteur impulsif","2.1a");
+		nbErreurs += addItemFilmOKTest ( sn, "Jean-Mi", "jeanjean", "Les tracteurs", "Thriller", "Jean-Pierre", "Bertrand", "4.1");
 		nbTests++;
-		nbErreurs += addMemberOKTest (sn, "Antoine", "antoine", "grand amoureux de la litt√©rature","2.1b");
+		nbErreurs += addItemFilmOKTest ( sn, "Gaetan", "azerty", "La bière", "Documentaire", "Hervé", "Jean-Claude", "4.2");
 		nbTests++;
-		nbErreurs += addMemberOKTest (sn, "Alice", "alice", "20 ans, sexy","2.1c");
+		nbErreurs += addItemFilmOKTest ( sn, "Mick", "12345", "La maisel", "Policier", "Vincent", "Franck", "4.3");
 
-		// tentative d'ajout de membre "existant"
-
-		nbTests++;
-		nbErreurs += addMemberAlreadyExistsTest(sn, new String("Paul"), "abcdefghij", "", "2.2", "L'ajout d'un membre avec le pseudo du premier membre ajout√© est accept√©");
-		nbTests++;
-		nbErreurs += addMemberAlreadyExistsTest(sn, new String("Alice"), "abcdefghij", "", "2.3", "L'ajout d'un membre avec le pseudo du dernier membre ajout√© est accept√©");
-		nbTests++;
-		nbErreurs += addMemberAlreadyExistsTest(sn, new String("anToine"), "abcdefghij", "", "2.4", "L'ajout d'un membre avec un pseudo existant (avec casse diff√©rente) est accept√©");
-		nbTests++;
-		nbErreurs += addMemberAlreadyExistsTest(sn, new String(" Antoine "), "abcdefghij", "", "2.5", "L'ajout d'un membre avec un pseudo existant (avec leading et trailing blanks) est accept√©");		
-
+		
+		
+		// tentative d'ajout de film avec identifiants erronés
 
 		nbTests++;
-		if (nbFilms != sn.nbFilms()) {
-			System.out.println("Erreur  :  le nombre de films apr√®s utilisation de addMember a √©t√© modifi√©");
-			nbErreurs++;
-		}
+		nbErreurs += addItemFilmNotMemberTest( sn, "Jean-Jacques", "jeanjean", "Les tracteurs", "Thriller", "Jean-Pierre", "Bertrand", "4.4", "L'ajout d'un film avec un pseudo n'existant pas est accepté");
+		nbTests++;
+		nbErreurs += addItemFilmNotMemberTest( sn, "Jean-Mi", "irefirg", "Les tracteurs 2", "Thriller", "Jean-Pierre", "Bertrand", "4.5", "L'ajout d'un film avec un couple id/pwd erroné est accepté");
+		
+		//tentative d'ajout de film déjà existant
+		
+		nbTests++;
+		nbErreurs += addItemFilmAlreadyExistsTest( sn, "Gaetan", "azerty", "  Les tracteurs ", "Western", "Jean-Pierre", "Bertrand", "4.6", "L'ajout d'un film avec le même nom que le premier ajouté est accepté");
+
+		
 		nbTests++;
 		if (nbLivres != sn.nbBooks()) {
-			System.out.println("Erreur  :  le nombre de livres apr√®s utilisation de addMember a √©t√© modifi√©");	
+			System.out.println("Erreur  :  le nombre de livres après utilisation de addItemFilm a été modifié");	
 			nbErreurs++;
 		}
 
@@ -163,7 +202,7 @@ public class TestsAddItemFilm {
 		System.out.println(sn);
 
 		// bilan du test de addMember
-		System.out.println("TestsAddMember :   " + nbErreurs + " erreur(s) / " +  nbTests + " tests effectu√©s");
+		System.out.println("TestsAddItemFilm :   " + nbErreurs + " erreur(s) / " +  nbTests + " tests effectu√©s");
 
 	}
 }
